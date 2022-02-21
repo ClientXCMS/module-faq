947e6f8a0df7a25afa03049e61050cf649493d1c
Martindev <Delebecque.martin@gmail.com>
2022-02-21 23:28:50 +0100

    Add permission and fix title

--- src/Faq/config.php
+++ src/Faq/config.php
@@ -1,12 +1,16 @@
 <?php
 
+use App\Faq\Actions\FaqCrudAction;
 use App\Faq\FAQAdminItem;
 use ClientX\Navigation\DefaultMainItem;
 use function DI\add;
 use function DI\get;
 
 return [
-    'navigation.main.items' => add([new DefaultMainItem([DefaultMainItem::makeItem('faq.title', 'faq.index', 'fa fa-question')], 80)]),
+    'navigation.main.items' => add([new DefaultMainItem([DefaultMainItem::makeItem('FAQ', 'faq.index', 'fa fa-question')], 80)]),
     "admin.menu.items" => add(get(FAQAdminItem::class)),
-    'csrf.except' => add('faq.admin.switch')
+    'csrf.except' => add('faq.admin.switch'),
+    'permissions.list' => add([
+        FaqCrudAction::class => 'faq'
+    ])
 ];
